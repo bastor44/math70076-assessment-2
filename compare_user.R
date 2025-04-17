7e9cb9e4d69faf43ccec4cc8746a818281bd0bd7
bastor44 <bastor1222@gmail.com>
2025-04-17 19:26:08 +0100

    fix filtering in plot_test_score function

--- compare_user.R
+++ compare_user.R
@@ -3,55 +3,69 @@
 require(tidyverse)
 require(ggplot2)
 
+test_names <- list("SAT_Math", "SAT_Reading",  "SAT_Writing", 
+                   "ACT_Composite", "ACT_Math", 
+                   "ACT_English", "ACT_Writing")
+
 ## plot SAT/ACT scores and add line for user 
-plot_test_score <- function(data=institutions, school, 
+plot_test_score <- function(data, school, 
                             test=c("SAT_Math", "SAT_Reading",  "SAT_Writing", 
-                                   "SAT_Average", "ACT_Composite", "ACT_Math", 
+                                   "ACT_Composite", "ACT_Math", 
                                    "ACT_English", "ACT_Writing"), 
                             user_score=NULL) {
   
   # filter data to only look @ school of interest 
-  school_data <- data[, data$INSTNM==school]
+  school_data <- data[data$INSTNM==school, ]
   
   # filter data to only look @ test of interest 
   if (test=="SAT_Math") {
     school_data <- school_data |>
-      select(colnames(school_data), "SATMT")
+      select(colnames(school_data)[str_detect(colnames(school_data), "SATMT")])
     x <- seq(200, 800, 1)
   }
   if (test=="SAT_Reading") {
     school_data <- school_data |>
-      select(colnames(school_data), "SATVR")
+      select(colnames(school_data)[str_detect(colnames(school_data), "SATVR")])
+      #select(colnames(school_data), "SATVR")
     x <- seq(200, 800, 1)
   }
   if (test=="SAT_Writing") {
     school_data <- school_data |>
-      select(colnames(school_data), "SATWR")
+      select(colnames(school_data)[str_detect(colnames(school_data), "SATWR")])
+      #select(colnames(school_data), "SATWR")
     x <- seq(200, 800, 1)
   } 
   if (test=="ACT_Composite") {
     school_data <- school_data |>
-      select(colnames(school_data), "ACTCM")
-    x <- seq(1, 36, 1)
+      select(colnames(school_data)[str_detect(colnames(school_data), "ACTCM")])
+      #select(colnames(school_data), "ACTCM")
+    x <- seq(1, 36, .1)
   }
   if (test=="ACT_MATH") {
     school_data <- school_data |>
-      select(colnames(school_data), "ACTMT")
-    x <- seq(1, 36, 1)
+      select(colnames(school_data)[str_detect(colnames(school_data), "ACTMT")])
+      #select(colnames(school_data), "ACTMT")
+    x <- seq(1, 36, .1)
   }
   if (test=="ACT_English") {
     school_data <- school_data |>
-      select(colnames(school_data), "ACTEN")
-    x <- seq(1, 36, 1)
+      select(colnames(school_data)[str_detect(colnames(school_data), "ACTEN")])
+      #select(colnames(school_data), "ACTEN")
+    x <- seq(1, 36, .1)
   }
   if (test=="ACT_Writing") {
     school_data <- school_data |>
-      select(colnames(school_data), "ACTWR")
-    x <- seq(2, 12, 1)
+      select(colnames(school_data)[str_detect(colnames(school_data), "ACTWR")])
+      #select(colnames(school_data), "ACTWR")
+    x <- seq(2, 12, .1)
   }
   
   if (any(is.na(school_data))){
-    return("Requested test information is unavailable.")
+    return(list(
+      plot=NULL, 
+      quantile="Requested test information is unavailable."
+        )
+      )
   }
   
   # extract mean, median, and quartiles 
@@ -74,17 +88,28 @@ plot_test_score <- function(data=institutions, school,
     labs(x="Test score", y="Density",
          title=paste(test, "score distribution at \n", school)) +
     geom_vline(xintercept=avg, color="grey70", linetype="dashed", linewidth=1) + 
-    geom_vline(xintercept=c(q25, q75), color="grey90", linetype="dashed", linewidth=1)
+    geom_vline(xintercept=c(q25, q75), color="grey90", linetype="dashed", linewidth=1) +
+    theme(title=element_text(size=16), 
+          axis.text=element_text(size=12), 
+          axis.title=element_text(size=14))
   
+  # add line to show user score
   if (!is.null(user_score)) {
     scores_plot <- scores_plot +
       geom_vline(xintercept=user_score, color="firebrick", linewidth=1)
+    user_quantile <- round(pnorm(user_score, avg, sd), 2)*100
   }
   
-  # user quantile
-  user_quantile <- qnorm(user_score, avg, sd)
+  if(is.null(user_score)){
+    user_quantile <- NA
+  }
   
-  return(list(plot=scores_plot, 
+  scores_plot <- scores_plot + scale_x_continuous()
+  
+  return(list(avg=avg,
+              q25=q25,
+              q75=q75,
+              plot=scores_plot, 
               quantile=user_quantile))
 }
 
